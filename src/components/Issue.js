45618612f9b34b0ced8646c0a8238203e23e2597
HAMISH <constatcopywriting@gmail.com>
2018-05-13 12:42:50 +1200

    Stable before edit for multi-issue state

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -6,25 +6,28 @@ import { getIssueDetails } from '../actions/contractActions'
 
 class Issue extends Component {
   render() {
-
-    return(
-      <div>
-        <h3>Issue: {this.props.issue.name}</h3>
-        <h3>submitter: {this.props.issue.submitter}</h3>
-        <h3>acceptor: {this.props.issue.acceptor}</h3>
-        <h3>arbitrator: {this.props.issue.arbitrator}</h3>
-        <h3>arbitratorFee: {this.props.issue.arbitratorFee}</h3>
-        <h3>accepted: {this.props.issue.accepted ? 'true' : 'false'}</h3>
-        <h3>resolved: {this.props.issue.resolved ? 'true' : 'false'}</h3>
-        <h3>funds: {this.props.issue.funds}</h3>
-      </div>
-    )
+    return this.renderIssueDetails(this.props.issue)
   }
 
   async componentDidMount() {
     const { getIssueDetails, disputeAddress } = this.props
 
     getIssueDetails(disputeAddress, 0)
+  }
+
+  renderIssueDetails = issue => {
+    return(
+      <div>
+        <h3>Issue: {issue.name}</h3>
+        <h3>submitter: {issue.submitter}</h3>
+        <h3>acceptor: {issue.acceptor}</h3>
+        <h3>arbitrator: {issue.arbitrator}</h3>
+        <h3>arbitratorFee: {issue.arbitratorFee}</h3>
+        <h3>accepted: {issue.accepted ? 'true' : 'false'}</h3>
+        <h3>resolved: {issue.resolved ? 'true' : 'false'}</h3>
+        <h3>funds: {issue.funds}</h3>
+      </div>
+    )
   }
 }
 
